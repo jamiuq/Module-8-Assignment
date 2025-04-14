8451ec88e66a48bd6c1203f5fc5506de1b855315
jamiuq <jamiuq@farmingdale.edu>
2025-04-13 23:45:05 -0400

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Grade
+{
+   public static void main(String[] args)
+   {
+     String input;
+     int questions;
+     int missed;
+     double minPassing;
+
+     input = JOptionPane.showInputDialog("How many questions are on the exam?");
+     questions = Integer.parseInt(input);
+
+     input = JOptionPane.showInputDialog("How many questions did the student miss?");
+     missed = Integer.parseInt(input);
+
+     input = JOptionPane.showInputDialog("What is the minimum passing score?");
+     minPassing = Integer.parseInt(input);
+
+     PassFailExam exam = new PassFailExam(questions, missed, minPassing);
+     JOptionPane.showMessageDialog(null, "Each question counts " + exam.getPointsEach() + " points.\nThe exam score is " + exam.getScore() + ".\nThe exam grade is " + exam.getGrade() + ".");
+   }
+}
